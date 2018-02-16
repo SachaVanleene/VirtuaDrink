3eaeca51e5d19dbc128b6ddde2f5ec633dbf97ff
Matt Elbé <matt.leboucher@gmail.com>
2018-02-16 12:56:56 -0500

    Adds alcohol UI/management.

---
+++ Assets/UI/AlcoholManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlcoholManager : MonoBehaviour
+{
+    public delegate void OnRateChangeDelegate(float newRate);
+    public event OnRateChangeDelegate OnRateChange;
+
+	/*
+	 * The current alcohol rate.
+	 */
+    private float alcoholRate;
+
+    [SerializeField]
+    public float AlcoholRate
+    {
+        get
+        {
+            return alcoholRate;
+        }
+
+        set
+        {
+            if (alcoholRate == value)
+                return;
+
+            alcoholRate = value;
+
+            if (OnRateChange != null)
+                OnRateChange(alcoholRate);
+        }
+    }
+
+	[Tooltip("The slider reflecting a graphical representation of the alcohol rate.")]
+    [SerializeField]
+    private Slider slider;
+
+	[Tooltip("The text displaying the alcohol rate.")]
+    [SerializeField]
+    private Text text;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        alcoholRate = 0.5f;
+        OnRateChange += UpdateUI;
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            AlcoholRate += 0.1f;
+    }
+
+	/// <summary>
+    /// Updates the alcohol UI part whenever the rate is updated.
+    /// </summary>
+    private void UpdateUI(float newRate)
+    {
+        slider.value = newRate;
+        text.text = newRate + " g";
+    }
+}
