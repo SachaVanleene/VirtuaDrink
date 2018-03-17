60d97fb0bd6c52fe077b0fe7f94a41b19200b017
Matt Elbé <matt.leboucher@gmail.com>
2018-03-17 18:03:12 -0400

    Car scripts.

---
+++ Assets/Car/Scripts/Car.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+	private Rigidbody rb;
+
+	[Header("Car Specs")]
+	public float wheelRadius;
+
+	// Use this for initialization
+	void Awake() {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
