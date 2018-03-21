4cf8079cf782b24231eca8ded8a5ecb04424f4b7
Matt Elbé <matt.leboucher@gmail.com>
2018-03-21 00:08:24 -0400

    Detects accident.

---
+++ Assets/AccidentDetector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccidentDetector : MonoBehaviour
+{
+	public GameObject accidentText;
+	
+	private void FixedUpdate()
+	{
+		if (Vector3.Dot(transform.up, Vector3.down) > 0)
+			accidentText.SetActive(true);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		accidentText.SetActive(true);
+	}
+}
