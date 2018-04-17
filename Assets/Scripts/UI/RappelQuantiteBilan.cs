0803f75a0f8c74be66b089284a6c8aa1ddee2315
Sacha vanleen <sacha1231@hotmail.fr>
2018-04-17 13:37:48 -0400

    AJout rappel quantite consommé dans la scène bilan

---
+++ Assets/Scripts/UI/RappelQuantiteBilan.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RappelQuantiteBilan : MonoBehaviour {
+
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+        text.text = "Vous aviez ingéré " + AlcoolQuantity.alcool_quantity.ToString(".0##") + " g/cl";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
